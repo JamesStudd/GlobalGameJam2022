152c0686c796caff835a94374c64d3e1fe1caba5
James Studd <james.studd@live.co.uk>
2022-01-29 15:03:28 +0100

    Adds line renderer that still needs to be looking betterer

--- Assets/_Scripts/MovementPlayback.cs
+++ Assets/_Scripts/MovementPlayback.cs
@@ -6,20 +6,23 @@ using UnityEngine;
 public class MovementPlayback : MonoBehaviour
 {
     [SerializeField] private float _pauseTime;
+    [SerializeField] private float _moveBackTime = 3f;
 
     private Vector3 _startPosition;
-    
+
     private bool _isRec = false;
     private float _tempX;
     private float _tempY;
     private float _tempZ;
-    
+
     private readonly List<float> _nums = new List<float>();
 
     private PlayerInput _playerInput;
 
     public event Action OnReplayed;
-    
+    public event Action OnStartRecording;
+    public event Action OnEndRecording;
+
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
@@ -32,38 +35,34 @@ public class MovementPlayback : MonoBehaviour
     {
         _startPosition = transform.position;
         _isRec = true;
+        OnStartRecording?.Invoke();
     }
 
-    [ContextMenu("StopRecording")]
-    public void StopRecording()
-    {
-        _isRec = false;
-        transform.position = _startPosition;
-    }
-    
     [ContextMenu("Replay")]
     public void Replay()
     {
         _isRec = false;
         StartCoroutine(Playback());
+        
+        OnEndRecording?.Invoke();
     }
-    
+
     [ContextMenu("Reset")]
     public void Reset()
     {
         _nums.Clear();
     }
-    
+
     private void FixedUpdate()
     {
         if (!_isRec) return;
-        
+
         var position = transform.position;
-            
+
         _tempX = position.x;
         _tempY = position.y;
         _tempZ = position.z;
-            
+
         _nums.Add(_tempX);
         _nums.Add(_tempY);
         _nums.Add(_tempZ);
@@ -72,20 +71,25 @@ public class MovementPlayback : MonoBehaviour
     private IEnumerator Playback()
     {
         var initial = true;
-        
+
         while (true)
         {
             if (initial)
             {
-                for (int i = _nums.Count - 1; i >= 0; i -= 3)
+                float elapsedTime = 0;
+                Vector3 startingPos = transform.position;
+                while (elapsedTime < _moveBackTime)
                 {
-                    transform.position = new Vector3(_nums[i - 2], _nums[i - 1], _nums[i - 0]); 
-                    yield return new WaitForFixedUpdate();
+                    transform.position = Vector3.Lerp(startingPos, _startPosition, (elapsedTime / _moveBackTime));
+                    elapsedTime += Time.deltaTime;
+                    yield return new WaitForEndOfFrame();
                 }
                 
+                transform.position = _startPosition;
+
                 OnReplayed?.Invoke();
             }
-            
+
             for (int i = 0; i < _nums.Count; i += 3)
             {
                 transform.position = new Vector3(_nums[i], _nums[i + 1], _nums[i + 2]);
---
+++ Assets/_Scripts/PlayerLineRenderer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+    public class PlayerLineRenderer : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer _lineRenderer;
+        [SerializeField] private float _recordInterval;
+
+        private int _index = 0;
+        private float _nextRecord;
+        private MovementPlayback _movementPlayback;
+
+        private bool _canRecord = false;
+        
+        private void Awake()
+        {
+            _movementPlayback = GetComponent<MovementPlayback>();
+            
+            _movementPlayback.OnStartRecording +=  () => _canRecord = true;
+            _movementPlayback.OnEndRecording +=  () => _canRecord = false;
+            _movementPlayback.OnReplayed += () => _lineRenderer.enabled = true;
+
+            _lineRenderer.enabled = false;
+            _lineRenderer.SetPositions(new Vector3[0]);
+        }
+
+        private void Update()
+        {
+            if (_canRecord && Time.realtimeSinceStartup > _nextRecord)
+            {
+                _nextRecord = Time.realtimeSinceStartup + _recordInterval;
+                RecordPoint();
+            }
+        }
+
+        private void RecordPoint()
+        {
+            _lineRenderer.positionCount = _index + 1;
+            _lineRenderer.SetPosition(_index, transform.position);
+            _index++;
+        }
+    }
+}
