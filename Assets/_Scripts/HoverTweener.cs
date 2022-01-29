7c05da34a37675b72e55fd23217eae254b31c193
James Studd <james.studd@live.co.uk>
2022-01-29 17:53:55 +0100

    Makes end game betererererererer

---
+++ Assets/_Scripts/HoverTweener.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class HoverTweener : MonoBehaviour
+    {
+        [SerializeField] private Transform _root;
+        [SerializeField] private float _time;
+        [SerializeField] private float _amount;
+        [SerializeField] private Ease _ease;
+
+        private void Awake()
+        {
+            var position = _root.localPosition;
+            
+            var target = position + new Vector3(position.x, position.y + _amount, position.z);
+            
+            _root.DOLocalMove(target, _time)
+                .SetEase(_ease)
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+}
---
+++ Assets/_Scripts/RotateTweener.cs
@@ -0,0 +1,59 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace _Scripts
+{
+    public class RotateTweener : MonoBehaviour
+    {
+        [SerializeField] private float _time;
+        [SerializeField] private Ease _ease;
+        [SerializeField] private Axis _affectedAxis = Axis.Z;
+        
+        [Range(-1, 1)]
+        [SerializeField] private float _direction = 1f;
+
+        private Tweener _tweener;
+
+        private void Awake()
+        {
+            var rotation = new Vector3(
+                GetDegreeForAxis(Axis.X),
+                GetDegreeForAxis(Axis.Y),
+                GetDegreeForAxis(Axis.Z)
+            );
+
+            _tweener = transform.DOLocalRotate(rotation, _time, RotateMode.LocalAxisAdd)
+                .SetEase(_ease)
+                .SetLoops(-1, LoopType.Incremental);
+        }
+
+        private void LateUpdate()
+        {
+            if (_tweener == null)
+            {
+                return;
+            }
+
+            if (!gameObject.activeSelf && _tweener.IsPlaying())
+            {
+                _tweener.Pause();
+            }
+            else if (!_tweener.IsPlaying())
+            {
+                _tweener.Play();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _tweener?.Kill();
+        }
+
+        private float GetDegreeForAxis(Axis axis)
+        {
+            return (_affectedAxis.HasFlag(axis) ? 360 : 0) * _direction;
+        }
+    }
+}
