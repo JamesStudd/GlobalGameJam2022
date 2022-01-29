0f7af32d9eacb9588d3a8c833f83413153c3373c
James Studd <james.studd@live.co.uk>
2022-01-29 16:58:26 +0100

    Adds matrix style particle effect

---
+++ Assets/_Scripts/PlayerParticleEffects.cs
@@ -0,0 +1,20 @@
+﻿using Sirenix.Utilities;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class PlayerParticleEffects : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem[] _particleSystems;
+
+        private MovementPlayback _movementPlayback;
+
+        private void Awake()
+        {
+            _movementPlayback = GetComponent<MovementPlayback>();
+
+            _movementPlayback.OnEndRecording += () => _particleSystems.ForEach(e => e.Play());
+            _movementPlayback.OnReplayed += () => _particleSystems.ForEach(e => e.Stop());
+        }
+    }
+}
