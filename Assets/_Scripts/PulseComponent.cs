ed7ef44013b7bffdce38bfc469e9644ab76c54e2
James Studd <james.studd@live.co.uk>
2022-01-30 15:08:07 +0100

    Adds text saying to press space to continue

---
+++ Assets/_Scripts/PulseComponent.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class PulseComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _root;
+        [SerializeField] private float _scaleAmount;
+        [SerializeField] private float _scaleTime;
+        
+        private void Awake()
+        {
+            var pos = transform.localScale;
+            var endVal = new Vector3(pos.x * _scaleAmount, pos.y * _scaleAmount, pos.z * _scaleAmount);
+            
+            _root.DOScale(endVal, _scaleTime)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetLink(gameObject, LinkBehaviour.KillOnDestroy);
+        }
+    }
+}
